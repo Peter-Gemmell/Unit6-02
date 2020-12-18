a936dc023efd64206153bf410078ceca5f63571a
Peter Gemmell <peter.gemmell@ocsbstudent.ca>
2020-12-18 15:12:36 +0000

    downie

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function myFunction() {
-  var x, text;
+function myFunction () {
+  const x, text
 
-  var x = document.getElementById("numb").value;
+  const x = document.getElementById('numb').value
 
- if (isNaN(x)) { 
-   text = "input is not a number";
-  } else if  (x < 0) {
-   text = "Input is a negative integer";
- } else { 
-   text = "Input is a positive integer";
- } 
- document.getElementById("demo").innerHTML = text;
+  if (isNaN(x)) { 
+    text = 'Input is not a number'
+  } else if (x < 0) {
+    text = 'Input is a negative integer'
+  } else { 
+    text = 'Input is a positive integer'
+  } 
+  document.getElementById('demo').innerHTML = text;
 }
